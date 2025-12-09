24fd54ff14de244f4c1e680f46b59ad32c43feef
Sumeyya Aktas <celestinedior1225@gmail.com>
2025-12-09 18:47:27 -0500

    fix text alignment issue

--- kernel/kernel.asm
+++ kernel/kernel.asm
@@ -5,48 +5,32 @@ VIDEO_MEMORY equ 0xb8000
 WHITE_ON_BLACK equ 0x0f
 
 kernel_entry:
-    mov byte [VIDEO_MEMORY], ' '
-    mov byte [VIDEO_MEMORY + 1], 0x0f    
-
-    mov edi, VIDEO_MEMORY + 2
-    mov ecx, (80 * 25) - 1
+    mov edi, VIDEO_MEMORY
+    mov ecx, 80 * 25
     mov ax, 0x0F20              
     rep stosw
 
-    mov eax, [VIDEO_MEMORY]
-
     mov esi, kernel_msg
-    mov edi, VIDEO_MEMORY + 2
+    mov edi, VIDEO_MEMORY
     call print_string
-
-    mov esi, usb_msg
-    mov edi, VIDEO_MEMORY + 160
-    call print_string
-
-    mov eax, [VIDEO_MEMORY + 160]
 
     cli
     hlt
     jmp $
 
 print_string:
-    push eax
-    push edi
-
-.loop:
     lodsb
     cmp al, 0
     je .done
+    
     mov ah, WHITE_ON_BLACK
     stosw
-    jmp .loop
+    
+    jmp print_string
     
 .done:
-    pop edi
-    pop eax
     ret
 
-kernel_msg: db 'KERNEL LOADED! Hello from 32-bit protected mode!', 0
-usb_msg: db 'Initializing PCI enumeration for USB EHCI driver...', 0
+kernel_msg: db 'KERNEL: Hello from the kernel! USB driver coming soon...', 0
 
-times 8192 - ($ - $$) db 0
+times 8192 - ($ - $$) db 0      
