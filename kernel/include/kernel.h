9381452cae063f4f6844e6e23b94de8a0d4011ef
Sumeyya Aktas <celestinedior1225@gmail.com>
2025-12-13 20:04:21 -0500

    add print hex func

--- kernel/include/kernel.h
+++ kernel/include/kernel.h
@@ -5,7 +5,8 @@
 #define WHITE_ON_BLACK 0x0F
 
 void print(const char* str, int row);
+void print_hex(uint32_t value, int row);
 void clear_screen(void);
 void kernel_main(void);
 
-#endif KERNEL_H
+#endif 
