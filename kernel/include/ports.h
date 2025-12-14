ef2db9172556f081ec0856342cbfef4031d1457d
Sumeyya Aktas <celestinedior1225@gmail.com>
2025-12-13 20:13:31 -0500

    add outb/inb ports

--- kernel/include/ports.h
+++ kernel/include/ports.h
@@ -1,26 +1,35 @@
 #ifndef PORTS_H
 #define PORTS_H
 
-static inline unsigned char inb(unsigned short port) {
-    unsigned char result;
-    __asm__("inb %1, %0" : "=a" (result) : "Nd" (port));
-    return result;
-}
-
-static inline unsigned short inw(unsigned short port) {
+static inline unsigned short inw(unsigned short port) 
+{
     unsigned short result;
     __asm__("inw %1, %0" : "=a" (result) : "Nd" (port));
     return result;
 }
 
-static inline unsigned int inl(unsigned short port) {
+static inline unsigned int inl(unsigned short port) 
+{
     unsigned int result;
-    __asm__("inl %1, %0" : "=a" (result) : "Nd" (port));
+    __asm__ __volatile__("inl %1, %0" : "=a"(result) : "Nd"(port) : "memory");
     return result;
 }
 
-static inline void outl(unsigned short port, unsigned int data) {
-    __asm__("outl %0, %1" : : "a" (data), "Nd" (port));
+static inline void outl(unsigned short port, unsigned int data) 
+{
+    __asm__ __volatile__("outl %0, %1" : : "a"(data), "Nd"(port) : "memory");
+}
+
+static inline void outb(uint16_t port, uint8_t val)
+{
+    __asm__ volatile("outb %b0, %w1" : : "a"(val), "Nd"(port) : "memory");
+}
+
+static inline uint8_t inb(uint16_t port)
+{
+    uint8_t ret;
+    __asm__ volatile("inb %w1, %b0" : "=a"(ret) : "Nd"(port) : "memory");
+    return ret;
 }
 
 #endif
