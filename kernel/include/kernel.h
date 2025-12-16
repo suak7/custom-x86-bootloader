c121532ff0f78929099cf46ac9529ed6da94f7d1
Sumeyya Aktas <celestinedior1225@gmail.com>
2025-12-15 23:50:25 -0500

    clean up unused funcs

--- kernel/include/kernel.h
+++ kernel/include/kernel.h
@@ -4,8 +4,6 @@
 #define VIDEO_MEMORY 0xB8000
 #define WHITE_ON_BLACK 0x0F
 
-void print(const char* str, int row);
-void print_hex(uint32_t value, int row);
 void clear_screen(void);
 void kernel_main(void);
 
