6d4ceed2d8177e7d66f7180aa8c55f748a6ed7da
Sumeyya Aktas <celestinedior1225@gmail.com>
2025-12-17 00:44:18 -0500

    tweak serial init

--- kernel/include/driver/serial.h
+++ kernel/include/driver/serial.h
@@ -2,10 +2,11 @@
 #define SERIAL_H
 
 #include <stdint.h>
+#include <stdbool.h>
 
 #define COM1 0x3F8
 
-void serial_init(void);
+bool serial_init(void);
 void serial_write_char(char c);
 void serial_print(const char* str);
 void serial_print_hex(uint32_t value);
